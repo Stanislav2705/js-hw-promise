d2deb62571570d82d2d6ac2bc874a83b86fc8c0e
Stanislav2705 <stasrogal25@gmail.com>
2022-08-01 16:54:53 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
+import '../css/common.css';
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyColor: document.querySelector('body'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const timer = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      refs.bodyColor.style.backgroundColor = getRandomHexColor();
+
+      console.log(getRandomHexColor());
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener('click', () => {
+  timer.start();
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  timer.stop();
+});
